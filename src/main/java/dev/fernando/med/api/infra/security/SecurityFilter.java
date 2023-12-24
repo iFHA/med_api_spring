14b10bc776c5c04556d49aaf061d8b475bc2066e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-12-24 18:49:51 -0300

    validando token recebido do usuário

---
+++ src/main/java/dev/fernando/med/api/infra/security/SecurityFilter.java
@@ -0,0 +1,36 @@
+package dev.fernando.med.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+        var subject = this.tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não encontrado no cabeçalho Authorization.");
+        }
+        var tokenJWT = authorizationHeader.replace("Bearer ", "");
+        if(tokenJWT.isEmpty()) {
+            throw new RuntimeException("Token JWT vazio.");
+        }
+        return tokenJWT;
+    }
+}
--- src/main/java/dev/fernando/med/api/infra/security/TokenService.java
+++ src/main/java/dev/fernando/med/api/infra/security/TokenService.java
@@ -3,6 +3,9 @@ package dev.fernando.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import dev.fernando.med.api.models.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,6 +28,21 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar o token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    // specify an specific claim validations
+                    .withIssuer("API med")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!", exception);
         }
     }
 
